d4a4a0acac4438ec65bd3eed93f4dcaacbfd976d
Valentin Amiache-Fauré <valentinamiache@hotmail.fr>
2021-04-06 15:06:43 +0200

    Added visited case and final array containing solution.

--- script.js
+++ script.js
@@ -1,75 +1,89 @@
-$.getJSON("labyrinthes.json", function(data) {
-
+$.getJSON("labyrinthes.json", display)
+let maze1;
+// const tab2D = [];
+function display(data) {
     let size = 25;
-    let maze1 = data[size]['ex-2'];
-    console.log(maze1)
+    maze1 = data[size]['ex-2'];
+    // console.log(maze1)
     maze = mazeConstruction(maze1, size);
-    DFSite(maze, maze[0],size)
-});
+    // for(let elem of maze1){
+    //     if(tab2D[elem.posX]  === undefined){
+    //         tab2D.push([])
+    //     }
+    //     tab2D[elem.posX].push(elem)
+    // }
+    // console.log(tab2D[10][5]);
+    console.log("oui")
+    DFSite(maze, maze[0], size)
+}
 
 
 
-     let mazeConstruction = (maze1, size) => {
-         let caseNum = 0;
+let mazeConstruction = (maze1, size) => {
+    let caseNum = 0;
+
+    maze1.forEach(cell => {
+        let walls_str = cell.walls.map(x => x ? "1px" : "0px").join(" ");
+        let className = ["structure", cell.posX, cell.posY].join("-");
+        $(".labyrinthe").append(`<div class="case ${className}"></div>`);
+        cell.case = caseNum;
+        $(`.${className}`).css("border", "solid black");
+        $(`.${className}`).css("border-width", walls_str);
+        $(`.${className}`).attr("id", `case-${caseNum++}`)
+        cell.visited = false;
 
 
 
-         maze1.forEach(cell => {
-             let walls_str = cell.walls.map(x => x ? "1px" : "0px").join(" ");
-             let className = ["structure", cell.posX, cell.posY].join("-");
-             let div = document.createElement("div");
-             $(".labyrinthe").append(`<div class="item ${className}"></div>`);
-             // div.setAttribute("id", `case-${caseNum++}`)
-             cell.case = caseNum;
-             $(`.${className}`).css("border", "solid black");
-             $(`.${className}`).css("border-width", walls_str);
+    })
+    document.getElementsByClassName("labyrinthe")[0].style.gridTemplateColumns = `repeat(${size}, 50px)`;//Renvoie un objet de type tableau de tous les éléments enfants qui ont tous les noms de classe donnés
+    return maze1;
 
-             cell.visited = false;
-
-
-         })
-         document.getElementsByClassName("labyrinthe")[0].style.gridTemplateColumns = `repeat(${size}, 50px)`;//Renvoie un objet de type tableau de tous les éléments enfants qui ont tous les noms de classe donnés
-         return maze1;
-
- }
-let getNeighborsNotVisited = (currentcase, maze1, size) => {
-    let neighborsNotVisited = [];
-    let wallFormula = [(currentcase.case - size), (currentcase.case + 1), (currentcase.case + size), (currentcase.case - 1)];
+}
+let getNextNotVisited = (currentcase, maze1, size) => {
+    let nextNotVisited = [];
+    let wallForm = [(currentcase.case - size), (currentcase.case + 1), (currentcase.case + size), (currentcase.case - 1)];
     let i = 0;
     for (const wall of currentcase['walls']) {
         if (!wall) {
-            neighborsNotVisited.push(maze1[wallFormula[i]]);
+            nextNotVisited.push(maze1[wallForm[i]]);
         }
         i++;
     }
-    console.log(currentcase);
-    return neighborsNotVisited;
+    // console.log(currentcase);
+    return nextNotVisited;
 
 }
-let DFSite=(maze1,currentcase,size)=> {
-    let goal = (size*size)-1;
+let DFSite = (maze1, currentcase, size) => {
+    let end = (size * size) - 1;
     let stack = [];
     stack.push(currentcase);
+
+    let ch = []
 
     while (stack.length > 0) {
         currentcase = stack.pop();
         if (currentcase.visited === false) {
             currentcase.visited = true
-            if(currentcase.case===goal){
+            if (currentcase.case === end) {
 
-                console.log("vous êtes sortie!")
-                document.getElementById(`case-${currentcase.case}`).classList.add("winnercolor");
+                console.log("end")
+                document.getElementById(`case-${currentcase.case}`).classList.add("winner");
+                console.log(stack);
                 return currentcase.case;
             }
 
 
-            getNeighborsNotVisited(currentcase,maze1,size).forEach(neighbor =>{
+            getNextNotVisited(currentcase, maze1, size).forEach(neighbor => {
                 stack.push(neighbor);
             });
-            document.getElementById(`case-${currentcase.case}`).classList.add("visitedcolor");
+            document.getElementById(`case-${currentcase.case}`).classList.add("visited");
+            ch.push(currentcase)
+
 
         }
 
     }
-}
 
+    console.log(ch)
+
+}
